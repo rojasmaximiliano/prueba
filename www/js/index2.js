80d1ea40dd56dd019561c7494c6cecb6c96af8da
Maximiliano Rojas <maxi.rojasc@gmail.com>
2016-02-03 17:52:24 -0300

    con escanner

--- www/js/index2.js
+++ www/js/index2.js
@@ -28,6 +28,7 @@ var app = {
 			
 		// Leemos por ajax el archivos opcion1.html de la carpeta opciones
 		xhReq.open("GET", "opciones/opcion1.html", false);
+		getMensajes();
 		xhReq.send(null);
 		document.getElementById("contenidoCuerpo").innerHTML=xhReq.responseText;
 
@@ -104,6 +105,10 @@ function menu(opcion){
 			removeClass('li-menu-activo' , document.getElementById("ulMenu").getElementsByTagName("li")[opcion]);
 		}, 300);
 		
+		if (opcion==1){
+		getMensajes();
+		}
+		
 	    if (opcion ==2){
 		navigator.geolocation.getCurrentPosition(onSuccess, onError); 
 	 }
@@ -120,90 +125,170 @@ function onError(error) {
     alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n'); 
 }
 
-function scanear(){ 
-	alert("vamos a intentar");
-	cordova.plugins.barcodeScanner.scan( 
-		function (result) {  
-			console.log(result.text);
-			alert(result.text); 
-		}, 
-		function (error) { 
-			alert("nope");
-			notificacion("Ha ocurrido un error al escanear."); 
-		} 
-	); 
-}; 
+
 
 function initializeMap(lat,long) { 
-    myLatlng = new google.maps.LatLng(lat, long); 
+	myLatlng = new google.maps.LatLng(lat, long); 
 	myLatlng2 = new google.maps.LatLng(-33.4587173,-70.6623188);
     var mapOptions = { 
     zoom: 14, 
     center: myLatlng 
     }; 
-    
-	var puntos=[];
-	puntos = devolverPuntos();
-    map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 	
-    var marker = new google.maps.Marker({ 
-    position: myLatlng, 
-    map: map, 
-    title: 'Mi punto en el mapa' 
-    }); 
-	                
+	
+	map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
-	var cluster = new MarkerClusterer(map, puntos);
-			
-	var parentElement = document.getElementById("cargando");
-	parentElement.setAttribute('style', 'display:none;');
+	var key = "etkSJjrEJHyMcU5HL3YYyu0xru87e_fjsrbHYSi3z-nsgVzFu82K-fj6";
+                
+    //url = "http://localhost:8080/yoreciclo/puntos/obtener/"+key
+	url= "http://10.42.207.205:8080/yoreciclo/puntos/obtener/etkSJjrEJHyMcU5HL3YYyu0xru87e_fjsrbHYSi3z-nsgVzFu82K-fj6"
+    data =  key
+     $.ajax({
+        url: url,
+        dataType: 'json',
+        type : "GET",
+        data: data,
+        crossDomain: true,
+		timeout:60000,
+        complete: function(xhr, statusText){ 
+            console.log(xhr.responseText);
+        },
+					
+        success: function(data) {
+            if (data != null){
+				var table = document.getElementById("tablaPuntos");
+				for(var i in data){ 
+				
+					latLong= new google.maps.LatLng(data[i].latitud, data[i].longitud);
+					var marker = new google.maps.Marker({ 
+					position: latLong, 
+					map: map, 
+					title: data[i].tipo
+					}); 
+		
+					var row = table.insertRow(data.length);
+					var cell1 = row.insertCell(0);
+					cell1.innerHTML = "<td><div class='pure-u-3-4'><div id='encabezado'><b>"+data[i].direccion+"</b></div><div id = 'descripcion'>"+data[i].tipo+"</div></div><div class='pure-u-1-4'></div></td>";
+				}
+			}
+						
+			else{
+				alert('No hay información para mostrar');
+				}
+        },
+                    
+		error: function( req, status, err ) {
+            alert('Error establecer conexión');
+			var table = document.getElementById("tablaPuntos");
+			var row = table.insertRow(data.length);
+			var cell1 = row.insertCell(0);
+			cell1.innerHTML = "<td><div class='pure-u-3-4'><div id = 'descripcion'>Error en el servidor. Por favor, inténtelo de nuevo</div></div><div class='pure-u-1-4'></div></td>";
+            
+            }
+        });
+		
+		var parentElement = document.getElementById("cargando");
+		parentElement.setAttribute('style', 'display:none;');
+								
 } 
 
-function devolverPuntos(){
-
-            //$('#registro').submit(function() {
-                // recolecta los valores que inserto el usuario
-				alert('vamoavel');
-				var key = "etkSJjrEJHyMcU5HL3YYyu0xru87e_fjsrbHYSi3z-nsgVzFu82K-fj6";
+function getMensajes(){
+	var key = "etkSJjrEJHyMcU5HL3YYyu0xru87e_fjsrbHYSi3z-nsgVzFu82K-fj6";
                 
-                //url = "http://localhost:8080/yoreciclo/puntos/obtener/"+key
-				url= "http://10.42.207.205:8080/yoreciclo/puntos/obtener/etkSJjrEJHyMcU5HL3YYyu0xru87e_fjsrbHYSi3z-nsgVzFu82K-fj6"
-                data =  key
-				alert('dadfadsf');
-                $.ajax({
-                    url: url,
-                    dataType: 'json',
-                    type : "GET",
-                    data: data,
-                    crossDomain: true,
-					timeout:60000,
-                   complete: function(xhr, statusText){ 
-                        console.log(xhr.responseText);
-                    },
+    //url = "http://localhost:8080/yoreciclo/puntos/obtener/"+key
+	url= "http://10.42.207.205:8080/yoreciclo/mensaje/misMensajes/vokeyJVVDocGaFDG6l1VJ_Psr085yaRoMS4FwQ2PmZjrOYYSg0UAx4rC"
+    data =  key
+     $.ajax({
+        url: url,
+        dataType: 'json',
+        type : "GET",
+        data: data,
+        crossDomain: true,
+		timeout:60000,
+        complete: function(xhr, statusText){ 
+            console.log(xhr.responseText);
+        },
 					
-                    success: function(data) {
-                    	if (data != null){
-							alert('adsfaf');
-							$("#result").html(""); 
-							
-							var puntos = [];
-							for(var i in data){ 
-								$("#result").append("<li>"+data[i].direccion+"</li>"); 
-								var latLong= new google.maps.LatLng(data[i].latitud, data[i].longitud);
-								var punto = new google.maps.Marker({position:latLong})
-								puntos.push(punto);
-							}
-							return puntos; 
-						}
-						else{
-							alert('nooooo');
-						}
-                    },
-                    error: function( req, status, err ) {
-                        alert('Error establecer conexión');
-                    }
-                });
-				alert('whaaaaaat');
-                return false;
+        success: function(data) {
+            if (data != null){
+				var table = document.getElementById("tablaMensajes");
+				for(var i in data){ 
+				var fecha= new Date(data[i].fecha);
+				
+				var minutos = fecha.getMinutes();
+				minutos = minutos > 9 ? minutos : '0' + minutos;
+				/*
+				var dia = fecha.getDate();
+				var hora = fecha.getHours();
+				var minutos = fecha.getMinutes();
+				*/
+				
+					var row = table.insertRow(data.length);
+					var cell1 = row.insertCell(0);
+					cell1.innerHTML = "<td><div class='pure-u-3-4'><div id='encabezado'><b>"+data[i].textoMensaje+"</b></div><div id = 'descripcion'>El "+fecha.getDate()+"/"+fecha.getMonth()+1+" a las "+fecha.getHours()+":"+minutos+"</div></div><div class='pure-u-1-4'></div></td>";
+				}
+			}
+						
+			else{
+				alert('No hay información para mostrar');
+				}
+        },
+                    
+		error: function( req, status, err ) {
+            alert('Error establecer conexión');
+			var table = document.getElementById("tablaMensajes");
+			var row = table.insertRow(data.length);
+			var cell1 = row.insertCell(0);
+			cell1.innerHTML = "<td><div class='pure-u-3-4'><div id = 'descripcion'>Aún no tiene mensajes para mostrar</div></div><div class='pure-u-1-4'></div></td>";
             }
+        });
+
+}
+
+function scanear(){ 
+	alert("Debes colocar tu teléfono de forma horizontal para escanear");
+	var envio = new Object();
+	envio.tipo =  $("#tipo").val()
+	envio.key = key;
+	envio.idPuntoLimpioString = '1';
+	cordova.plugins.barcodeScanner.scan( 
+		function (envio) {  
+			key= "vokeyJVVDocGaFDG6l1VJ_Psr085yaRoMS4FwQ2PmZjrOYYSg0UAx4rC"
+			url= "http://10.42.207.205:8080/yoreciclo/"
+			data =  envio
+			alert(data);
+			 $.ajax({
+				url: url,
+				dataType: 'json',
+				type : "POST",
+				data: data,
+				crossDomain: true,
+				timeout:60000,
+				complete: function(xhr, statusText){ 
+					console.log(xhr.responseText);
+				},
+							
+				success: function(data) {
+					if (data!=null){
+						alert(data);
+						javascript:menu('1');
+					}
+					
+					else{
+						alert('No hay información para mostrar');
+						estado="menuprincipal";
+						menu('1');
+						}
+				},
+							
+				error: function( req, status, err ) {
+					alert('Error establecer conexión');
+					}
+				});
+		}, 
+		function (error) { 
+			notificacion("Ha ocurrido un error al escanear. Por favor intentalo de nuevo."); 
+		} 
+	); 
+}; 
 			
